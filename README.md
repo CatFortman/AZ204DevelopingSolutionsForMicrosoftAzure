17c214d297b95bc2366fa3873da86ff00ca81eee
Cat <catherine.fortman@tks.ca>
2024-02-03 12:57:39 -0400

    Complete skill 1.1

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# AZ-204-DevelopingSolutionsForMicrosoftAzure
+# Study Resource: Microsoft Azure 204 Exam: Developing Solutions for Microsoft Azure
+
+## Introduction
+
+Welcome to the repository for skills and labs related to the Microsoft Azure 204 exam: Developing Solutions for Microsoft Azure. This repository is intended to serve as a resource for those preparing for the exam, and it is based on the content from the exam reference book Developing Solutions for Microsoft Azure 2021 edition.
+
+### Study Resources
+
+- **Official Microsoft Study Guide:**
+  - [Microsoft Azure 204 Exam Study Guide](https://learn.microsoft.com/en-us/credentials/certifications/resources/study-guides/az-204)
+
+- **Official Labs:**
+  - [Microsoft Azure 204 Exam Labs](https://microsoftlearning.github.io/AZ-204-DevelopingSolutionsforMicrosoftAzure/)
+
+- **Additional Study Guide Created by Thomas Maurer:**
+  - [Thomas Maurer's AZ-204 Study Guide](https://www.thomasmaurer.ch/2020/03/az-204-study-guide-developing-solutions-for-microsoft-azure/)
+
+## Skills
+
+Here is a list of skills implemented in this repository that are covered in Developing Solutions for Microsoft Azure 2021 edition:
+
+* Skill 1.1: Implement solutions that use virtual machines (COMPLETED)
+* Skill 1.2: Create Azure App Service web apps
+* Skill 1.3: Implement Azure functions
+* Skill 2.1: Develop solutions that use Cosmos DB storage
+* Skill 2.2: Develop solutions that use Blob Storage
+* Skill 3.1: Implement user authentication and authorization
+* Skill 3.2: Implement secure cloud solutions
+* Skill 4.1: Implement caching and content delivery within solutions
+* Skill 4.2: Instrument solutions that support monitoring and logging
+* Skill 5.1: Develop an App Service Logic App
+* Skill 5.2: Implement API Management
+* Skill 5.3: Develop event-based solutions
+* Skill 5.4: Develop message-based solutions
+
+
+## Labs
+
+Here is a list of the hands-on labs implemented in this repository that are provided by Microsoft for practical experience:
+
+* Lab 01: Build a web application on Azure platform as a service offerings
+* Lab 02: Implement task processing logic by using Azure Functions
+* Lab 03: Retrieve Azure Storage resources and metadata by using the Azure Storage SDK for .NET
+* Lab 04: Construct a polyglot data solution
+* Lab 05: Deploy compute workloads by using images and containers
+* Lab 06: Authenticate by using OpenID Connect, MSAL, and .NET SDKs
+* Lab 07: Access resource secrets more securely across services
+* Lab 08: Create a multi-tier solution by using Azure services
+* Lab 09: Publish and subscribe to Event Grid events
+* Lab 10: Asynchronously process messages by using Azure Service Bus Queues
+* Lab 11: Monitor services that are deployed to Azure
+* Lab 12: Enhance a web application by using the Azure Content Delivery Network
+* Lab 13 (Optional): Create and configure an Azure web app
+* Lab 14 (Optional): Implement containerized solutions  
+
+  
+\
+Feel free to clone this repository and use the code and resources for your exam preparation. Good luck with your studies!
---
+++ Skills/1.1-provisioning-a-vm/Program1_1_1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace ch1_1_1
+{
+
+    class Program1_1_1
+    {
+        // Uncomment to run skill 1.1
+        // static void Main(string[] args)
+        // {
+        //     // create the management client. This will be used for all the operations
+        //     // performed in Azure.
+        //     var credentials = SdkContext.AzureCredentialsFactory.FromFile("./azureauth.properties");
+
+        //     var azure = Azure.Configure()
+        //     .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+        //     .Authenticate(credentials)
+        //     .WithDefaultSubscription();
+
+        //     // We need tocreate a resource group where we will add all the resources.
+        //     // Needed for the virtual machine.
+        //     var groupName = "az04-ResourceGroup";
+        //     var vmName = "az04VMTesting";
+        //     var location = Region.USEast2;
+        //     var vNetName = "az204VNet";
+        //     var vNetAddress = "172.16.0.0/16";
+        //     var subnetName = "az204Subnet";
+        //     var subnetAddress = "172.16.0.0/24";
+        //     var nicName = "az204NIC";
+        //     var adminUser = "azureadminuser";
+        //     var password = "pa$$w0rd!2019";
+
+        //     System.Console.WriteLine($"Creating resource group {groupName}...");
+        //     var ResourceGroup = azure.ResourceGroups.Define(groupName)
+        //     .WithRegion(location)
+        //     .Create();
+
+        //     // Every vm needs to be connected to virtual network.
+        //     System.Console.WriteLine($"Creating vm network {vNetName}...");
+        //     var network = azure.Networks.Define(vNetName)
+        //     .WithRegion(location)
+        //     .WithExistingResourceGroup(groupName)
+        //     .WithAddressSpace(vNetAddress)
+        //     .WithSubnet(subnetName, subnetAddress)
+        //     .Create();
+
+        //     // Any virtual machine needs a network interface for connecting to
+        //     // the virtual network.
+        //     System.Console.WriteLine($"Creating network interface {nicName}...");
+        //     var nic = azure.NetworkInterfaces.Define(nicName)
+        //     .WithRegion(location)
+        //     .WithExistingResourceGroup(groupName)
+        //     .WithExistingPrimaryNetwork(network)
+        //     .WithSubnet(subnetName)
+        //     .WithPrimaryPrivateIPAddressDynamic()
+        //     .Create();
+
+        //     // Create the vm.
+        //     System.Console.WriteLine($"Creating virtual machine {vmName}...");
+        //     azure.VirtualMachines.Define(vmName)
+        //     .WithRegion(location)
+        //     .WithExistingResourceGroup(groupName)
+        //     .WithExistingPrimaryNetworkInterface(nic)
+        //     .WithLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-DataCenter")
+        //     .WithAdminUsername(adminUser)
+        //     .WithAdminPassword(password)
+        //     .WithComputerName(vmName)
+        //     .WithSize(VirtualMachineSizeTypes.StandardDS2V2)
+        //     .Create();
+        // }
+    }
+}
